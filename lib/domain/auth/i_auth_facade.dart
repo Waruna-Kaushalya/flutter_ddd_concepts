a7fd9dc45c722153da164f2f69d2083fd719212d
Waruna-Kaushalya <waruna95610@gmail.com>
2022-02-19 18:55:29 +0530

    commit

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_ddd_concepts/domain/auth/value_objects.dart';
+
+import 'auth_failure.dart';
+
+//this is like reposotory
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
