7cefbf4988115e85b43441afbee165ec9d47c415
Waruna-Kaushalya <waruna95610@gmail.com>
2022-02-20 22:52:47 +0530

    commit

--- lib/application/auth/sign_in_form/bloc/sign_in_form_state.dart
+++ lib/application/auth/sign_in_form/bloc/sign_in_form_state.dart
@@ -7,6 +7,21 @@ class SignInFormState with _$SignInFormState {
     required Password password,
     required bool showErrorMessages,
     required bool isSubmitting,
+
+    /// why [Option]
+    ///
+    /// Bacause in initially there is no response untill user press button.
+    ///
+    /// [Either<AuthFailure, Unit>] in here Right side means sucssesfull
+    /// sign in and Left side means unsucssful sign in.
+    ///
+    /// we also need to have way to just no sign in happend yet.
+    ///
+    /// [Option<None,Some>]
+    /// none means no value s present, some means some value is present
+    ///
+    /// Then we can ui to in initlly Option<None>. That means sign in not
+    /// happend yet
     required Option<Either<AuthFailure, Unit>> authFailureOrSuccessOption,
   }) = _SignInFormState;
 
