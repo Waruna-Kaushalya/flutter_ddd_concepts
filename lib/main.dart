4cef475ad2ae760da21bb3d4ba199ed9a5425e68
Waruna-Kaushalya <waruna95610@gmail.com>
2022-02-26 21:03:45 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ddd_concepts/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
 
 import 'injection.dart';
@@ -7,40 +8,4 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies(Environment.prod);
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo"),
-      ),
-      body: const Center(),
-    );
-  }
 }
