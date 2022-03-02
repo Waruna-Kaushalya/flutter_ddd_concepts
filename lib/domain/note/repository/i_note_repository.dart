f7997dfd6bbb0f53abea23234795fd56a6f0618a
Waruna-Kaushalya <waruna95610@gmail.com>
2022-03-02 13:55:27 +0530

    INoteRepository

--- lib/domain/note/repository/i_note_repository.dart
+++ lib/domain/note/repository/i_note_repository.dart
@@ -1,12 +1,18 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_ddd_concepts/domain/note/entities/note.dart';
+import 'package:flutter_ddd_concepts/domain/note/failure/note_failure.dart';
+import 'package:kt_dart/collection.dart';
 
 abstract class INoteRepository {
   /// [Read]
   /// watch note
   /// watch uncompleted notes
   ///
-  /// [Update]
-  /// [Delete]
+  /// [Create] [Update] [Delete]
 
-  // Stream<Either<>>
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll();
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
+  Future<Either<NoteFailure, Unit>> create(Note note);
+  Future<Either<NoteFailure, Unit>> update(Note note);
+  Future<Either<NoteFailure, Unit>> delete(Note note);
 }
