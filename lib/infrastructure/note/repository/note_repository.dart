4b45e2d4a22076e9217f06779c139edddf1646fc
Waruna-Kaushalya <waruna95610@gmail.com>
2022-03-04 18:14:02 +0530

    firestore error fix

--- lib/infrastructure/note/repository/note_repository.dart
+++ lib/infrastructure/note/repository/note_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_ddd_concepts/infrastructure/note/dto/note_dtos.dart';
 import 'package:injectable/injectable.dart';
 
@@ -7,6 +10,7 @@ import 'package:flutter_ddd_concepts/domain/note/entities/note_entity.dart';
 import 'package:flutter_ddd_concepts/domain/note/failure/note_failure.dart';
 import 'package:flutter_ddd_concepts/domain/note/repository/i_note_repository.dart';
 import 'package:kt_dart/kt.dart';
+import 'package:rxdart/rxdart.dart';
 import '../../core/firestore_helpers.dart';
 
 @LazySingleton(as: INoteRepository)
@@ -19,43 +23,44 @@ class NoteRepository implements INoteRepository {
   Stream<Either<NoteFailure, KtList<NoteEntity>>> watchAll() async* {
     final userDoc = await _firebaseFirestore.userDocument();
 
-    userDoc.noteColletion
-        .orderBy("serverTimeStamp", descending: true)
-        .snapshots()
-        .map((snapshot) => right(
-              snapshot.docs
-                  .map((doc) => NoteDTO.fromFirestore(doc).toDomain())
-                  .toImmutableList(),
-            ));
-    // userDoc.noteColletion
-    //     .orderBy('serverTimeStamp', descending: true)
-    //     .snapshots()
-    //     .map(
-    //       (snapshot) => right(
-    //         snapshot.docs
-    //             .map((doc) => NoteDTO.fromFirestore(doc).toDomain())
-    //             .toImmutableList(),
-    //       ),
-    //     );
-    // userDoc.noteColletion
-
-    //     /// order by "serverTimeStamp" {sort}
+    // userDoc
+    //     .collection("notes")
     //     .orderBy("serverTimeStamp", descending: true)
     //     .snapshots()
-
-    //     /// [QuerySnapshot] map in to [Either<NoteFailure, KtList<NoteEntity>>]
-    //     /// [map] function always recive corect data
     //     .map((snapshot) => right(
-    //           /// [snapshot.docs] has List of QueryDocumentSnapshot
     //           snapshot.docs
-
-    //               /// and we want to map [QueryDocumentSnapshot] in to [KtList] of NoteEntity
-    //               .map((doc) => NoteDTO.fromFirestore(doc)
-
-    //                   /// [NoteDTO] map to domain
-    //                   .toDomain())
+    //               .map((doc) => NoteDTO.fromFirestore(doc).toDomain())
     //               .toImmutableList(),
     //         ));
+
+    yield* userDoc
+        .collection("notes")
+
+        /// order by "serverTimeStamp" {sort}
+        .orderBy("serverTimeStamp", descending: true)
+        .snapshots()
+
+        /// [QuerySnapshot] map in to [Either<NoteFailure, KtList<NoteEntity>>]
+        /// [map] function always recive corect data
+        .map((snapshot) => right<NoteFailure, KtList<NoteEntity>>(
+              /// [snapshot.docs] has List of QueryDocumentSnapshot
+              snapshot.docs
+
+                  /// and we want to map [QueryDocumentSnapshot] in to [KtList] of NoteEntity
+                  .map((doc) => NoteDTO.fromFirestore(doc)
+
+                      /// [NoteDTO] map to domain
+                      .toDomain())
+                  .toImmutableList(),
+            ))
+        .onErrorReturnWith((e, st) {
+      if (e is PlatformException && e.message!.contains('PERMISSION_DENIED')) {
+        return const Left(NoteFailure.insufficientPermissions());
+      } else {
+        log(e.toString());
+        return const Left(NoteFailure.insufficientPermissions());
+      }
+    });
   }
 
   @override
