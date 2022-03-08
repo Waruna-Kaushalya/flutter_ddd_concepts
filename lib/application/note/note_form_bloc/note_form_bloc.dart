bd184d949f21065e6183a7fc0b20b788cb298600
Waruna-Kaushalya <waruna95610@gmail.com>
2022-03-08 19:39:23 +0530

    check

--- lib/application/note/note_form_bloc/note_form_bloc.dart
+++ lib/application/note/note_form_bloc/note_form_bloc.dart
@@ -68,6 +68,7 @@ class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
               ? await _noteRepository.update(state.note)
               : await _noteRepository.create(state.note);
         }
+        //vsdvsvsd
 
         emit(state.copyWith(
           isSaving: false,
