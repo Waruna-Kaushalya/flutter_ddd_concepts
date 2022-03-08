85fc8fd4b723e744b672365c434ab723bec16e37
Waruna-Kaushalya <waruna95610@gmail.com>
2022-03-08 23:50:05 +0530

    Page

--- lib/presentation/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview/notes_overview_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ddd_concepts/application/auth/auth_bloc/bloc.dart';
 
 class NotesOverviewPage extends StatelessWidget {
   static const routeName = '/notesoverviewpage';
@@ -8,7 +10,27 @@ class NotesOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notes"),
+        leading: IconButton(
+          icon: const Icon(Icons.exit_to_app),
+          onPressed: () {
+            context.read<AuthBloc>().add(const AuthEvent.signOut());
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.indeterminate_check_box),
+          ),
+        ],
+      ),
       body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to note form page
+        },
+      ),
     );
   }
 }
